6c5bdde0398238860fbefd21848b06095d08da82
willli <willli@staritgp.com>
2018-11-03 16:37:09 +0800

    deleted

--- src/test1.js
+++
@@ -1,9 +0,0 @@
-let express = require("express")
-let app = express()
-
-app.get("/",(request, response) => {
-	response.send("Hello,world!")
-})
-app.listen(8000, () => {
-	console.log("Server Start at port : 8000")
-})
