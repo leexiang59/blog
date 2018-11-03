f64986c313ec51f3d9b5a42f206861d05d25f662
willli <willli@staritgp.com>
2018-11-03 16:24:24 +0800

    init

---
+++ src/test1.js
@@ -0,0 +1,9 @@
+let express = require("express")
+let app = express()
+
+app.get("/",(request, response) => {
+	response.send("Hello,world!")
+})
+app.listen(8000, () => {
+	console.log("Server Start at port : 8000")
+})
