0b2f81749530b4b49f48a9905b79d103c680999e
willli <willli@staritgp.com>
2018-11-06 20:04:57 +0800

    文章模块

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
-var blogList = require('./blog-list')
-var userList = require('./user/user-list')
+
+var userList = require('./user/')
+var article = require('./article/')
 var apiPath = {
     root:'/api/',
-    user:`/api/user/`
+    user:`/api/user/`,
+    article:`/api/article/`,
 }
 module.exports = function (app) {
 
-    app.get(`${apiPath.root}blog-list`, blogList);
-
+    // 用户信息
     app.get(`${apiPath.user}list`, userList.get);
     app.post(`${apiPath.user}add`, userList.add);
     app.post(`${apiPath.user}edit`, userList.edit);
     app.post(`${apiPath.user}delete`, userList.delete);
+
+    // 文章
+    app.get(`${apiPath.article}list`, article.get);
 };
