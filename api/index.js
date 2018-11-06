e68cfab7b8e612a5f927042938056166ef0d313f
willli <willli@staritgp.com>
2018-11-06 19:40:01 +0800

    用户接口 - 路径优化

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 var blogList = require('./blog-list')
 var userList = require('./user/user-list')
+var apiPath = {
+    root:'/api/',
+    user:`/api/user/`
+}
 module.exports = function (app) {
 
-    app.get('/api/blog-list', blogList);
+    app.get(`${apiPath.root}blog-list`, blogList);
 
-    app.get('/api/user/user-list', userList.get);
-    app.post('/api/user/user-add', userList.add);
-    app.post('/api/user/user-edit', userList.edit);
-    app.post('/api/user/user-delete', userList.delete);
+    app.get(`${apiPath.user}list`, userList.get);
+    app.post(`${apiPath.user}add`, userList.add);
+    app.post(`${apiPath.user}edit`, userList.edit);
+    app.post(`${apiPath.user}delete`, userList.delete);
 };
